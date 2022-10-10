98d3584b3a5a6892470347bf4571e0e949e0cbbe
Dec <dec@GentleStorms-Air.local>
2022-10-10 15:08:55 +0100

    added endpoint to cancel transaction

--- src/routes/walletRoute.ts
+++ src/routes/walletRoute.ts
@@ -1,16 +1,31 @@
 import { Router } from "express";
-import { updateWallet } from "../controller/walletController";
+import { cancelTx, updateWallet } from "../controller/walletController";
 import { userRequest } from "../types/express";
 import { auth } from "../utils/authMiddleware";
 
 const router = Router();
 
-router.patch("/", auth, async (req:userRequest, res) => {
+router.patch("/confirm", auth, async (req:userRequest, res) => {
 	try {
 		const data = req.body;
 		const id = req.user.user_id;
-		
 		const response = await updateWallet(data, id);
+		return res.status(200).json({
+			message: "Success",
+			response
+		})
+	} catch (error) {
+		console.log(error)
+		return res.status(400).json({
+			message: error
+		})
+	}
+});
+router.patch("/cancel", auth, async (req:userRequest, res) => {
+	try {
+		const txId = req.body.txId;
+		const id = req.user.user_id;
+		const response = await cancelTx(txId, id);
 		return res.status(200).json({
 			message: "Success",
 			response
